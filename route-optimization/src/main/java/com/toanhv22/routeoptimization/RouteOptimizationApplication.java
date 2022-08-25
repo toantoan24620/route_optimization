dc67764865a296b4caef989752683d22b7169d8c
toanhv22 <toantoan24620@gmail.com>
2022-08-25 21:44:20 +0700

    init project

---
+++ route-optimization/src/main/java/com/toanhv22/routeoptimization/RouteOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.toanhv22.routeoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteOptimizationApplication.class, args);
+	}
+
+}
---
+++ route-optimization/src/test/java/com/toanhv22/routeoptimization/RouteOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toanhv22.routeoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
